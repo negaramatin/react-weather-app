191ac0a16e8c0915b80dd93b6a4d80e84a5ff0ca
negaramatin <negarmatin20@gmail.com>
2023-10-08 20:33:38 +0330

    Added the GitHub repo URL

--- src/App.js
+++ src/App.js
@@ -2,7 +2,25 @@ import React from "react";
 import "./App.css";
 
 export default function App() {
-  return <div className="App">Weather app</div>;
+  return (
+    <div className="App">
+      <h1>Weather app</h1>
+      <footer>
+        This project was coded by{" "}
+        <a
+          href="https://www.linkedin.com/in/negar-matin-b7a38a261/"
+          target="_blank"
+        >
+          Negar Matin
+        </a>{" "}
+        and is{" "}
+        <a
+          href="https://github.com/negaramatin/react-weather-app"
+          target="_blank"
+        >
+          open-soureced on GitHub
+        </a>
+      </footer>
+    </div>
+  );
 }
-
-
