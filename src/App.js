bdc0131e2ad3e15e99d07ab12fd18fdb118f23e8
negaramatin <negarmatin20@gmail.com>
2023-10-09 11:56:24 +0330

    Added bootstrap

--- src/App.js
+++ src/App.js
@@ -4,25 +4,27 @@ import "./App.css";
 export default function App() {
   return (
     <div className="App">
-      <h1>Weather app</h1>
-      <footer>
-        This project was coded by{" "}
-        <a
-          href="https://www.linkedin.com/in/negar-matin-b7a38a261/"
-          target="_blank"
-          rel="noreferrer"
-        >
-          Negar Matin
-        </a>{" "}
-        and is{" "}
-        <a
-          href="https://github.com/negaramatin/react-weather-app"
-          target="_blank"
-          rel="noreferrer"
-        >
-          open-soureced on GitHub
-        </a>
-      </footer>
+      <div className="container">
+        <h1>Weather app</h1>
+        <footer>
+          This project was coded by{" "}
+          <a
+            href="https://www.linkedin.com/in/negar-matin-b7a38a261/"
+            target="_blank"
+            rel="noreferrer"
+          >
+            Negar Matin
+          </a>{" "}
+          and is{" "}
+          <a
+            href="https://github.com/negaramatin/react-weather-app"
+            target="_blank"
+            rel="noreferrer"
+          >
+            open-soureced on GitHub
+          </a>
+        </footer>
+      </div>
     </div>
   );
 }
