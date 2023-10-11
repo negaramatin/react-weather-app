08ed49d878ece3db68c28ae50bfa9a0746a48c5d
negaramatin <negarmatin20@gmail.com>
2023-10-11 12:25:03 +0330

    Added Weather Icon and Date

--- src/App.js
+++ src/App.js
@@ -1,3 +1,4 @@
+
 import React from "react";
 import Weather from "./Weather";
 import "./App.css";
@@ -6,14 +7,14 @@ export default function App() {
   return (
     <div className="App">
       <div className="container">
-        <h1>Weather app</h1>
-        <Weather />
+        <Weather defaultCity="New York" />
+
         <footer>
           This project was coded by{" "}
           <a
             href="https://www.linkedin.com/in/negar-matin-b7a38a261/"
             target="_blank"
-            rel="noreferrer"
+            rel="noopener noreferrer"
           >
             Negar Matin
           </a>{" "}
@@ -21,9 +22,17 @@ export default function App() {
           <a
             href="https://github.com/negaramatin/react-weather-app"
             target="_blank"
-            rel="noreferrer"
+            rel="noopener noreferrer"
           >
-            open-soureced on GitHub
+            open-sourced on GitHub
+          </a>{" "}
+          and{" "}
+          <a
+            href="https://suspicious-beaver-111c4d.netlify.com/"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            hosted on Netlify
           </a>
         </footer>
       </div>
