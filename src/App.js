72c42f52a3d085296c3137bc22d22c4541a3ee0f
negaramatin <negarmatin20@gmail.com>
2023-10-13 10:50:28 +0330

    Update Netlify link

--- src/App.js
+++ src/App.js
@@ -28,7 +28,7 @@ export default function App() {
           </a>{" "}
           and{" "}
           <a
-            href="https://suspicious-beaver-111c4d.netlify.com/"
+            href="https://chipper-praline-21b91b.netlify.app/"
             target="_blank"
             rel="noopener noreferrer"
           >
